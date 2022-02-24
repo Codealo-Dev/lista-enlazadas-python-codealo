1075627d593cac034ca199f32d0483d12965a38a
Johnnie Tirado <tiradoyamin@gmail.com>
2022-02-24 02:34:23 +0000

    Finished solutions

--- lista_enlazada.py
+++ lista_enlazada.py
@@ -7,6 +7,9 @@ class Nodo:
   valor = None
   siguiente = None
 
+  def __init__(self, valor):
+    self.valor = valor
+
 """
  * Agregar al final de nuestra lista un nuevo
  * nodo con el valor pasado como parametro
@@ -19,25 +22,76 @@ class Nodo:
 """
 
 class ListaEnlazada:
-  raiz = None
-  num_nodos: 0
 
+  def __init__(self):
+    self.raiz = None
+    self.num_nodos = 0
   
   def agregar_al_final(self, valor):
-    raise NotImplementedError()
-
+    if not self.raiz:
+      self.raiz = Nodo(valor)
+    else:
+      cur = self.raiz
+      while cur.siguiente:
+        cur = cur.siguiente
+      cur.siguiente = Nodo(valor)
+    self.num_nodos = self.num_nodos + 1
   
   def agregar_despues_de(self, indice, valor):
-    raise NotImplementedError()
+    if indice > self.num_nodos:
+      return None
+    cur_indice = indice
+    cur = self.raiz
+    while cur_indice > 0:
+      cur = cur.siguiente
+      cur_indice = cur_indice - 1
+    nuevo_nodo = Nodo(valor)
+    nuevo_nodo.siguiente = cur.siguiente
+    cur.siguiente = nuevo_nodo
+    self.num_nodos = self.num_nodos + 1
     
   def borrar_indice(self, indice):
-    raise NotImplementedError()
+    if indice > self.num_nodos:
+      return None
+
+    if indice == 0:
+      self.raiz = self.raiz.siguiente  
+    else:
+      cur_indice = indice
+      prev = self.raiz
+      cur = prev.siguiente
+      while cur_indice > 0:
+        prev = cur
+        cur = cur.siguiente
+        cur_indice = cur_indice - 1
+      prev.siguiente = cur.siguiente
+    self.num_nodos = self.num_nodos - 1
     
   def contiene(self, valor):
-    raise NotImplementedError()
+    cur = self.raiz
+    while cur.siguiente:
+      if cur.valor == valor:
+        return True
+    return False
 
   def valor_de(self, indice):
-    raise NotImplementedError()
+    if indice > self.num_nodos:
+      return None
+    cur_indice = indice
+    cur = self.raiz
+    while cur_indice > 0:
+      cur = cur.siguiente
+      cur_indice = cur_indice - 1
+    return cur.valor
     
   def imprimir(self):
-    raise NotImplementedError()
+    if not self.raiz:
+      return ""
+    
+    resultado = []
+    cur = self.raiz
+    while cur.siguiente:
+      resultado.append(str(cur.valor))
+      cur = cur.siguiente
+    resultado.append(str(cur.valor))
+    return " => ".join(resultado)
--- lista_enlazada_test.py
+++ lista_enlazada_test.py
@@ -2,27 +2,29 @@ from lista_enlazada import ListaEnlazada
 
 lista = ListaEnlazada()
 
+print(lista)
+
 def test_agregar_al_final():
   lista.agregar_al_final(10)
   assert lista.raiz.valor == 10
   assert lista.num_nodos == 1
 
-lista.agregar_al_final(3)
-lista.agregar_al_final(2)
-lista.agregar_al_final(1)
-
 def test_valor_de():
+  lista.agregar_al_final(3)
+  lista.agregar_al_final(2)
+  lista.agregar_al_final(1)
+  print(lista.imprimir())
   assert lista.valor_de(3) == 1
   
 def test_agregar_despues_de():
-  assert lista.agregar_despues_de(2, 12)
+  lista.agregar_despues_de(2, 12)
   assert lista.valor_de(3) == 12
   
 def test_borrar_indice():
   lista.borrar_indice(0)
   assert lista.valor_de(0) == 3
   lista.borrar_indice(10)
-  assert lista.num_nodos == 3
+  assert lista.num_nodos == 4
   
 def test_contiene():
   assert lista.contiene(12)
